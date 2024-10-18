4db8a8896fc35c59741ab6afeee07506c7f4d30a
rekhavc25 <rekhaswamy05@gmail.com>
2024-10-18 12:36:08 -0400

    Update streamlit_app.py
    worked on ingredient_string variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,9 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
-
-    #st.write(ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
