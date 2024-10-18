bdb4b4f0bf74b62cad290ac0a50a12cbe0b77a8f
rekhavc25 <rekhaswamy05@gmail.com>
2024-10-18 12:42:58 -0400

    Update streamlit_app.py
    changes for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+
 # Write directly to the app
 st.title(":cup_with_straw: Example Streamlit App :cup_with_straw:")
 st.write(
@@ -41,4 +42,9 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
     
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
